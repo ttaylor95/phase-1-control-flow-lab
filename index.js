8d1c70ee23e34505345500d1ab406dc61ebd894c
Teanna Taylor <teannamtaylor95@gmail.com>
2022-05-18 19:38:14 -0700

    commit tree 3912372a6794ae240fbe1d2467970f731666423a

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+let results
+
+if (someValue <= 400) {
+  results = 'This one is on me!';
+
+} 
+else if (someValue <= 2000) {
+
+  results = 'That will be twenty bucks.'
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (someValue > 2500) {
+
+  results = 'No can do.'
 }
 
-function switchOnCharmFromTip(){
+else if (someValue > 2000) {
+
+  results = 'I will gladly take your thirty bucks.'
+}
+
+return results;
+
+  
+}
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+let NYC = ternaryCheckCity
+ 
+return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+ // need to add return in order to complete ternary expression
+
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+
+    break;
+
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+
+    default:
+   return 'Bye.';
+
+}
 }
